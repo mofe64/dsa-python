e9ce94c9e1bd50282557e4e34c3f2886d665ee3d
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-09-20 19:54:53 +0100

    refactored insertion sort

--- algorithms/sorts/insertion_sort.py
+++ algorithms/sorts/insertion_sort.py
@@ -3,16 +3,20 @@ from util import exchange
 
 def sort(a: list):
     size = len(a)
-    i = 0
-    while i < size:
-        j = i
-        while j > 0:
-            if a[j] < a[j-1]:
-                exchange(a, j, j-1)
+    index_of_current_value = 0
+    while index_of_current_value < size:
+        index_of_value_to_compare = index_of_current_value
+        # At this point we want to compare the current value with all previous values
+        # This is to ensure that all values to the left of the array are sorted properly
+        # So to get all previous values we decrement the index of current value till we get to 0
+        while index_of_value_to_compare > 0:
+            if a[index_of_value_to_compare] < a[index_of_value_to_compare-1]:
+                exchange(a, index_of_value_to_compare,
+                         index_of_value_to_compare-1)
             else:
                 break
-            j -= 1
-        i += 1
+            index_of_value_to_compare -= 1
+        index_of_current_value += 1
 
 
 x = [
